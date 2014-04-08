78ec7a1ea1253cc101fa827dfbd09c640e2b36eb
amrunha <amha.hakim@gmail.com>
2014-04-08 22:11:01 +0700

    Create data.cs

---
+++ data.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormCrud
+{
+    class data
+    {
+        private string nim, nama, alamat, telpon;
+        public string Nim
+        {
+            get { return nim; }
+            set { nim = value; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public string Alamat
+        {
+            get { return alamat; }
+            set { alamat = value; }
+        }
+        public string Telpon
+        {
+            get { return telpon; }
+            set { telpon = value; }
+        }
+    }
+}
